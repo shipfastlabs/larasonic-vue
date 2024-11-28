a97807e299e936e28cfcb58ceb66c70cd10e2d6b
Pushpak Chhajed <pushpak1300@gmail.com>
2024-11-29 02:26:20 +0530

    chore: improve eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,26 +4,15 @@ import eslintPluginTailwindCSS from 'eslint-plugin-tailwindcss'
 export default antfu({
   vue: true,
   typescript: false,
-  ignores: ['storage/logs/**/*'],
   formatters: {
-    /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
     css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
     html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
     markdown: 'prettier',
   },
-}, [...eslintPluginTailwindCSS.configs['flat/recommended']], {
+  ignores: ['storage/logs/**/*', '**/*.{yaml,yml,php}'],
+}, {
+  files: ['**/*.{js,vue,json,ts,tsx,md,html,blade}'],
+  ...eslintPluginTailwindCSS.configs['flat/recommended'].rules,
   rules: {
     'tailwindcss/no-custom-classname': 'off',
   },
