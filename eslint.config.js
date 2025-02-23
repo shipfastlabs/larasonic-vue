7d0874dffde597140f4b37e1db58cbe8f08e71b6
Pushpak Chhajed <pushpak1300@gmail.com>
2025-02-23 23:28:11 +0530

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import antfu from '@antfu/eslint-config'
-import eslintPluginTailwindCSS from 'eslint-plugin-tailwindcss'
 
 export default antfu({
   vue: true,
@@ -10,10 +9,4 @@ export default antfu({
     markdown: 'prettier',
   },
   ignores: ['storage/**/*', '**/*.{yaml,yml,php}', 'resources/js/Components/shadcn/**/*', 'public/**/*'],
-}, {
-  files: ['**/*.{js,vue,json,ts,tsx,md,html,blade}'],
-  ...eslintPluginTailwindCSS.configs['flat/recommended'].rules,
-  rules: {
-    'tailwindcss/no-custom-classname': 'off',
-  },
 })
